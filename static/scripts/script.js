60744904c482fd024a9597d9cf1bc9540873a5dd
Paul Leeming <pleeming@gmail.com>
2017-05-10 13:11:04 +0100

    Add iphone icon, read state of button to display on/off (unfinished)

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,3 +1,25 @@
+$(document).ready(function() {
+    showButtons('http://192.168.1.66', 'Lamp');
+});
+
+function showButtons(url, name) {
+    $.get(url + "/getstate", function(data) {
+        if (data === '1') {
+            $('#btn' + name + 'On').hide();
+            $('#btn' + name + 'Off').show();
+        }
+        if (data === '0') {
+            $('#btn' + name + 'On').show();
+            $('#btn' + name + 'Off').hide();
+        }
+    });
+}
+
+function setstate(url, state, name) {
+    $.post(url + '/setstate?state=' + state);
+    showButtons(url, name);
+}
+
 function createCORSRequest(method, url) {
     var xhr = new XMLHttpRequest();
     if ("withCredentials" in xhr) {
